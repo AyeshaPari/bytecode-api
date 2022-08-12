361f1aae04b169800d2dbff6c99ee86fd2bc9708
bytecode77 <mail@martinfischer.it>
2022-08-12 08:39:04 +0200

    + BytecodeApi.UI.Effects.InvertEffect

---
+++ BytecodeApi.UI/Effects/InvertEffect.cs
@@ -0,0 +1,38 @@
+﻿using BytecodeApi.UI.Extensions;
+using BytecodeApi.UI.Properties;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace BytecodeApi.UI.Effects
+{
+	/// <summary>
+	/// Provides a bitmap effect that inverts the colors of an image.
+	/// </summary>
+	public sealed class InvertEffect : ShaderEffect
+	{
+		/// <summary>
+		/// Identifies the <see cref="Input" /> dependency property. This field is read-only.
+		/// </summary>
+		public static readonly DependencyProperty InputProperty = RegisterPixelShaderSamplerProperty(nameof(Input), typeof(InvertEffect), 0);
+		/// <summary>
+		/// Gets or sets a <see cref="Brush" /> that applies an invert effect on the bitmap.
+		/// </summary>
+		public Brush Input
+		{
+			get => this.GetValue<Brush>(InputProperty);
+			set => SetValue(InputProperty, value);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="InvertEffect" /> class.
+		/// </summary>
+		public InvertEffect()
+		{
+			PixelShader = new PixelShader();
+			PixelShader.SetStreamSource(new MemoryStream(Resources.FileInvertEffect));
+			UpdateShaderValue(InputProperty);
+		}
+	}
+}
--- BytecodeApi.UI/Properties/Resources.Designer.cs
+++ BytecodeApi.UI/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace BytecodeApi.UI.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -66,6 +66,16 @@ namespace BytecodeApi.UI.Properties {
         internal static byte[] FileGrayscaleEffect {
             get {
                 object obj = ResourceManager.GetObject("FileGrayscaleEffect", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] FileInvertEffect {
+            get {
+                object obj = ResourceManager.GetObject("FileInvertEffect", resourceCulture);
                 return ((byte[])(obj));
             }
         }
